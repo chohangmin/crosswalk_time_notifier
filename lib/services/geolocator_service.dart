4b78a232ad239631e486dcada2c34efad3b71aa7
chohangmin <slightsmille@gmail.com>
2024-04-16 14:44:11 +0900

    Add geolocator funtion.

--- lib/services/geolocator_service.dart
+++ lib/services/geolocator_service.dart
@@ -1,62 +1,177 @@
 import 'dart:async';
 
+import 'package:flutter/rendering.dart';
 import 'package:geolocator/geolocator.dart';
 
 class GeolocatorService {
-  StreamSubscription<Position>? _positionStreamSubscription;
+  static const String kLocationServicesDisableMessage =
+      'Location services are disabled.';
+  static const String kPermissionDeniedMessage = 'Permission denied.';
+  static const String kPermissionDeniedForeverMessage =
+      'Permission denied forever.';
+  static const String kPermissionGrantedMessage = 'Permission granted.';
 
-  late final StreamController<Position> _positionStreamController =
-      StreamController<Position>.broadcast();
+  final GeolocatorPlatform geolocatorPlatform = GeolocatorPlatform.instance;
+  final List<PositionItem> positionItems = <PositionItem>[];
+  StreamSubscription<Position>? positionStreamSubscription;
+  StreamSubscription<ServiceStatus>? serviceStatusStreamSubscription;
+  bool positionStreamStarted = false;
 
-  void startListeningToPositionSream() {
-    const LocationSettings locationSettings = LocationSettings(
-      accuracy: LocationAccuracy.high,
-      distanceFilter: 100,
+  @override
+  void initState() {
+    super.initState();
+    toggleServiceStatusStream();
+  }
+
+  Future<void> getCurrentPosition() async {
+    final hasPermission = await handlePermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    final position = geolocatorPlatform.getCurrentPosition();
+
+    updatePositionList(
+      PositionItemType.position,
+      position.toString(),
+    );
+  }
+
+  Future<bool> handlePermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await geolocatorPlatform.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      updatePositionList(
+        PositionItemType.log,
+        kLocationServicesDisableMessage,
+      );
+
+      return false;
+    }
+
+    permission = await geolocatorPlatform.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await geolocatorPlatform.requestPermission();
+      if (permission == LocationPermission.denied) {
+        updatePositionList(
+          PositionItemType.log,
+          kPermissionDeniedMessage,
+        );
+        return false;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      updatePositionList(
+        PositionItemType.log,
+        kPermissionDeniedForeverMessage,
+      );
+
+      return false;
+    }
+
+    updatePositionList(
+      PositionItemType.log,
+      kPermissionGrantedMessage,
     );
 
-    _positionStreamSubscription =
-        Geolocator.getPositionStream(locationSettings: locationSettings).listen(
-      (Position? position) {
-        if (position != null) {
-          _positionStreamController.add(position);
+    return true;
+  }
+
+  void updatePositionList(PositionItemType type, String displayValue) {
+    positionItems.add(PositionItem(type, displayValue));
+    setstate(() {});
+  }
+
+  void toggleServiceStatusStream() {
+    if (serviceStatusStreamSubscription == null) {
+      final serviceStatusStream = geolocatorPlatform.getServiceStatusStream();
+      serviceStatusStreamSubscription =
+          serviceStatusStream.handleError((error) {
+        serviceStatusStreamSubscription?.cancel();
+        serviceStatusStreamSubscription = null;
+      }).listen((serviceStatus) {
+        String serviceStatusValue;
+        if (serviceStatus == ServiceStatus.enabled) {
+          if (positionStreamStarted) {
+            toggleListening();
+          }
+          serviceStatusValue = 'enabled';
+        } else {
+          if (positionStreamSubscription != null) {
+            setState(() {
+              positionStreamSubscription?.cancel();
+              positionStreamSubscription = null;
+              updatePositionList(
+                  PositionItemType.log, 'Position Stream has been canceled.');
+            });
+          }
+          serviceStatusValue = 'disabled';
         }
-        print(position == null
-            ? 'Unknown'
-            : 'Listend position : ${position.latitude.toString()}, ${position.longitude.toString()}');
-      },
-    );
+        updatePositionList(
+          PositionItemType.log,
+          'Location service has been $serviceStatusValue',
+        );
+      });
+    }
   }
 
-  void stopListeningToPositionStream() {
-    _positionStreamSubscription?.cancel();
+  void toggleListening() {
+    if (positionStreamSubscription == null) {
+      final positionStream = geolocatorPlatform.getPositionStream();
+      positionStreamSubscription = positionStream.handleError((error) {
+        positionStreamSubscription?.cancel();
+        positionStreamSubscription = null;
+      }).listen((position) => updatePositionList(
+            PositionItemType.position,
+            position.toString(),
+          ));
+      positionStreamSubscription?.pause();
+    }
+
+    setState(() {
+      if (positionStreamSubscription == null) {
+        return;
+      }
+      String statusDisplayValue;
+      if (positionStreamSubscription!.isPaused) {
+        positionStreamSubscription!.resume();
+        statusDisplayValue = 'resumed';
+      } else {
+        positionStreamSubscription!.pause();
+        statusDisplayValue = 'pause';
+      }
+
+      updatePositionList(
+        PositionItemType.log,
+        'Listening for position updates $statusDisplayValue',
+      );
+    });
   }
 
-  Stream<Position> get positionStream => _positionStreamController.stream;
+  @override
+  void dispose() {
+    if (positionStreamSubscription != null) {
+      positionStreamSubscription!.cancel();
+      positionStreamSubscription = null;
+    }
 
-  // Future<Position> determinePosition() async {
-  //   bool serviceEnabled;
-  //   LocationPermission permission;
+    super.dispose();
+  }
+}
 
-  //   serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  //   if (!serviceEnabled) {
-  //     return Future.error('Location services are disabled.');
-  //   }
+enum PositionItemType {
+  log,
+  position,
+}
 
-  //   permission = await Geolocator.checkPermission();
+class PositionItem {
+  PositionItem(this.type, this.displayValue);
 
-  //   if (permission == LocationPermission.denied) {
-  //     permission = await Geolocator.requestPermission();
-  //     if (permission == LocationPermission.denied) {
-  //       return Future.error('Location permissions are denied.');
-  //     }
-  //   }
-
-  //   if (permission == LocationPermission.deniedForever) {
-  //     return Future.error(
-  //         'Location permissions are parmanently denied, we cannote request permissions.');
-  //   }
-
-  //   return await Geolocator.getCurrentPosition(
-  //       desiredAccuracy: LocationAccuracy.high);
-  // }
+  final PositionItemType type;
+  final String displayValue;
 }
